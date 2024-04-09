3dc88f7d9206eeb97512be61cc17da45fd523672
Anatoly Samaris <anatolysamaris@gmail.com>
2024-04-09 22:23:36 +0300

    fix import

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from PyQt5.QtWidgets import QApplication
-from frontend.DrawingWindow import DrawingWindow
 import sys
+
+sys.path.append('./backend')
+sys.path.append('./frontend')
+
+
+from frontend.DrawingWindow import DrawingWindow
 
 
 if __name__ == "__main__":
