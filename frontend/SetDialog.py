6ce86e1be15f5dd7f194110667d52b2ddfec710d
Anatoly Samaris <anatolysamaris@gmail.com>
2024-04-21 00:08:46 +0300

    обработка ввода на задание выигрышей

--- frontend/SetDialog.py
+++ frontend/SetDialog.py
@@ -23,92 +23,89 @@ class SetDialog(QtWidgets.QDialog):
 "    border: 1px solid black;\n"
 "}")
         self.current_node = current_node
+        self.int_validator = QtGui.QIntValidator(-1000, 1000, self)
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(10)
+
         self.verticalLayoutWidget = QtWidgets.QWidget(self)
         self.verticalLayoutWidget.setGeometry(QtCore.QRect(10, 0, 211, 151))
         self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
         self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout.setObjectName("verticalLayout")
+
         spacerItem = QtWidgets.QSpacerItem(20, 10, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
         self.verticalLayout.addItem(spacerItem)
+
         self.label = QtWidgets.QLabel(self.verticalLayoutWidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
         self.label.setSizePolicy(sizePolicy)
-        font = QtGui.QFont()
-        font.setFamily("Arial")
-        font.setPointSize(10)
         self.label.setFont(font)
         self.label.setObjectName("label")
         self.verticalLayout.addWidget(self.label)
+
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setSizeConstraint(QtWidgets.QLayout.SetDefaultConstraint)
         self.horizontalLayout.setSpacing(3)
         self.horizontalLayout.setObjectName("horizontalLayout")
+
         self.label_2 = QtWidgets.QLabel(self.verticalLayoutWidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
         self.label_2.setSizePolicy(sizePolicy)
-        font = QtGui.QFont()
-        font.setFamily("Arial")
-        font.setPointSize(10)
         self.label_2.setFont(font)
         self.label_2.setObjectName("label_2")
         self.horizontalLayout.addWidget(self.label_2)
+
         self.lineEdit = QtWidgets.QLineEdit(self.verticalLayoutWidget)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.lineEdit.sizePolicy().hasHeightForWidth())
-        self.lineEdit.setSizePolicy(sizePolicy)
         self.lineEdit.setMaximumSize(QtCore.QSize(100, 25))
-        font = QtGui.QFont()
-        font.setFamily("Arial")
-        font.setPointSize(10)
         self.lineEdit.setFont(font)
         self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit.setValidator(self.int_validator)
+        self.lineEdit.setPlaceholderText("A")
         self.horizontalLayout.addWidget(self.lineEdit)
+
         self.label_3 = QtWidgets.QLabel(self.verticalLayoutWidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.label_3.sizePolicy().hasHeightForWidth())
         self.label_3.setSizePolicy(sizePolicy)
-        font = QtGui.QFont()
-        font.setFamily("Arial")
-        font.setPointSize(10)
         self.label_3.setFont(font)
         self.label_3.setObjectName("label_3")
         self.horizontalLayout.addWidget(self.label_3)
+
         self.lineEdit_2 = QtWidgets.QLineEdit(self.verticalLayoutWidget)
         self.lineEdit_2.setMaximumSize(QtCore.QSize(100, 25))
-        font = QtGui.QFont()
-        font.setFamily("Arial")
-        font.setPointSize(10)
         self.lineEdit_2.setFont(font)
         self.lineEdit_2.setObjectName("lineEdit_2")
+        self.lineEdit_2.setValidator(self.int_validator)
+        self.lineEdit_2.setPlaceholderText("B")
         self.horizontalLayout.addWidget(self.lineEdit_2)
+
         self.label_4 = QtWidgets.QLabel(self.verticalLayoutWidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.label_4.sizePolicy().hasHeightForWidth())
         self.label_4.setSizePolicy(sizePolicy)
-        font = QtGui.QFont()
-        font.setFamily("Arial")
-        font.setPointSize(10)
         self.label_4.setFont(font)
         self.label_4.setObjectName("label_4")
         self.horizontalLayout.addWidget(self.label_4)
+
         self.verticalLayout.addLayout(self.horizontalLayout)
+
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
         spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout_2.addItem(spacerItem1)
+
         self.pushButton = QtWidgets.QPushButton(self.verticalLayoutWidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
@@ -116,14 +113,12 @@ class SetDialog(QtWidgets.QDialog):
         sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
         self.pushButton.setSizePolicy(sizePolicy)
         self.pushButton.setMaximumSize(QtCore.QSize(100, 16777215))
-        font = QtGui.QFont()
-        font.setFamily("Arial")
-        font.setPointSize(10)
         self.pushButton.setFont(font)
         self.pushButton.setLayoutDirection(QtCore.Qt.LeftToRight)
         self.pushButton.setObjectName("pushButton")
         self.pushButton.pressed.connect(self.check_and_set_costs)
         self.horizontalLayout_2.addWidget(self.pushButton)
+
         self.verticalLayout.addLayout(self.horizontalLayout_2)
 
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
@@ -142,9 +137,15 @@ class SetDialog(QtWidgets.QDialog):
         self.y = y
         self.move(x, y)
 
+    def set_error(self):
+        style = 'border: 1px solid red;'
+        self.lineEdit.setStyleSheet(style)
+        self.lineEdit_2.setStyleSheet(style)
+
     def check_and_set_costs(self):
-        a, b = int(self.lineEdit.text()), int(self.lineEdit_2.text())
-        if a is not None and b is not None:
+        a, b = self.lineEdit.text(), self.lineEdit_2.text()
+        if a != '' and b != '':
+            a, b = int(a), int(b)
             if (not self.current_node.getChildren()
                 or not self.current_node.checkChildrenCosts()
                 or (a, b) == self.current_node.findBestCosts()):
@@ -152,9 +153,9 @@ class SetDialog(QtWidgets.QDialog):
                 self.parent().update()
                 self.close()
             else:
-                pass    # Обработка неправильных значений
+                self.set_error()
         else:
-            pass    # Обработка пустых значений
+            self.set_error()
 
 
 if __name__ == "__main__":
